ef1a43af6989456612904b539213f951f7f3fdb5
Diya-Lad <84093103+Diya-Lad@users.noreply.github.com>
2025-03-01 15:14:22 +0530

    Fix console output message for Checkin System Project

--- src/main/java/com/checkinsystemproject/Checkin/System/Project/CheckinSystemProjectApplication.java
+++ src/main/java/com/checkinsystemproject/Checkin/System/Project/CheckinSystemProjectApplication.java
@@ -8,7 +8,7 @@ public class CheckinSystemProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CheckinSystemProjectApplication.class, args);
-		System.out.println("Checkin System Project is running...");
+		System.out.println("Checkin System Project is running....");
 	}
 
 }
