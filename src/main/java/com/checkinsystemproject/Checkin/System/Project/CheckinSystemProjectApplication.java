b53733abb07c98467b232548e0968e801de7e838
Om Nai <93648778+OmNai12@users.noreply.github.com>
2025-03-01 00:41:00 -0800

    Project up and running...

---
+++ src/main/java/com/checkinsystemproject/Checkin/System/Project/CheckinSystemProjectApplication.java
@@ -0,0 +1,14 @@
+package com.checkinsystemproject.Checkin.System.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckinSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckinSystemProjectApplication.class, args);
+		System.out.println("Checkin System Project is running...");
+	}
+
+}
---
+++ src/test/java/com/checkinsystemproject/Checkin/System/Project/CheckinSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.checkinsystemproject.Checkin.System.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinSystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
